fbeea0b8003accb5a26dc32ec4e97aac80a083d4
Jibram <cajibram@gmail.com>
2019-09-28 17:39:35 -0700

    Push 0.0.2

--- src/components/StreamerView/TeamViewersView.jsx
+++ src/components/StreamerView/TeamViewersView.jsx
@@ -4,7 +4,7 @@ class TeamViewersView extends React.Component {
     render() {
         return(
             <div>
-                Here is a list of all of your viewers <3
+                
             </div>
         )
     }
--- src/util/Services/Services.js
+++ src/util/Services/Services.js
@@ -1,4 +1,5 @@
-const axios = require('axios').require;
+import axios from 'axios'
+import Authentication from '../../util/Authentication/Authentication'
 const CID = 'k9oz550zkxdteioqf5noq4ef4y0qq4'
 
 export function getUserName(user_id) {
@@ -6,13 +7,36 @@ export function getUserName(user_id) {
         return 'MaxGrosshandler'
     }
     else {
-        axios.get(``)
     }
 }
 
 export function isInTeam(user_id) {
-    axios.get(`/user/${user_id}}`, )
-        .then(function (response){
-            return (response.team_id != '')
+    return getTeamName(user_id) != '' 
+}
+
+// Team Management
+export function getTeamName(user_id) {
+    axios.get(`/user/${user_id}`)
+        .then(function (response) {
+            return response.team_id
+        })
+}
+
+export function createTeam(user_id, teamName) {
+    axios.get(`/user/${user_id}`, {
+        team_id: teamName
+        })
+        .then(function (response) {
+            return response
+        })
+}
+
+// Viewer Panel
+export function getRole(user_id) {
+    axios.get(`/team/${getTeamName(getChannelId())}`, {
+        user: user_id
+        })
+        .then(function (response) {
+            return response.role
         })
 }
