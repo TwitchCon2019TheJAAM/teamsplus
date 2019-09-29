25934781e8a894dfc5a4e678f3ff4d774c5933b4
MaxGrosshandler <maxgrosshandler@gmail.com>
2019-09-28 17:39:45 -0700

    did some tinkering

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -82,7 +82,7 @@ export default class App extends React.Component{
                         <p>My opaque ID is {this.Authentication.getOpaqueId()}.</p>
                         <div>{this.Authentication.isModerator() ? <p>I am currently a mod, and here's a special mod button <input value='mod button' type='button'/></p>  : 'I am currently not a mod.'}</div>
                         <p>I have {this.Authentication.hasSharedId() ? `shared my ID, and my user_id is ${this.Authentication.getUserId()}` : 'not shared my ID'}.</p>
-                   <AudienceView fakeList={["Streamer A", "Streamer B", "Streamer C"]} userID={"40664650"}></AudienceView>
+                   <AudienceView fakeList={["Streamer A", "Streamer B", "Streamer C"]} userID={this.Authentication.getUserId()}></AudienceView>
                     </div>
                 </div>
             )
--- src/util/Services/Services.js
+++ src/util/Services/Services.js
@@ -1,14 +1,7 @@
-const axios = require('axios').require;
+import axios from 'axios'
 const CID = 'k9oz550zkxdteioqf5noq4ef4y0qq4'
 
-export function getUserName(user_id) {
-    if (user_id == 40664650) {
-        return 'MaxGrosshandler'
-    }
-    else {
-        axios.get(``)
-    }
-}
+
 
 export function isInTeam(user_id) {
     axios.get(`/user/${user_id}}`, )
